d704cc21dad58560b0c14ef3d6ef8d24398ca704
Josh Butcher <sjwb1999@gmail.com>
2024-10-16 11:20:24 -0700

    update elements in file, added branches to button3.onclick

--- js/scripts.js
+++ js/scripts.js
@@ -1,27 +1,45 @@
 window.onload = function() {
   const button1 = document.getElementById("button-1");
-  let quiz_q1 = document.getElementById("quiz-q1");
-  let rules = document.getElementById("rules");
-  let joke = document.getElementById("rickRoll");
+  const button2 = document.getElementById("button-2");
+  const button3 = document.getElementById("button-3")
 
 
   
   button1.onclick = function() {
+    let quiz_q1 = document.getElementById("quiz-q1");
+    let rules = document.getElementById("rules");
+    let joke = document.getElementById("rickRoll");
+
     quiz_q1.setAttribute("class", "hidden");
     quiz_q1.removeAttribute("class");
     quiz_q1.setAttribute("class", "quiz-body");
     rules.setAttribute("class", "hidden");
     rules.removeAttribute("class");
     rules.setAttribute("class", "box");
+
+    joke.onclick = function() {
+      location.reload();
+    }
   }
 
-  joke.onclick = function() {
-    location.reload();
+  button2.onclick = function() {
+    let quiz_q2 = document.getElementById("quiz-q2");
+    quiz_q2.setAttribute("class", "hidden");
+    quiz_q2.removeAttribute("class");
+    quiz_q2.setAttribute("class", "quiz-body");
   }
 
-  button-Q2 = document.getElementById("button-q2");
+  button3.onclick = function() {
+    let scoreQ1 = document.getElementById("scoreQ1");
+    let scoreQ2 = document.getElementById("scoreQ2");
+    let num1 = parseInt(scoreQ1.value);
+    let num2 = parseInt(scoreQ2.value);
+    const sum = num1 + num2;
 
-  button-Q2.onclick = function() {
-    
+    if (sum >= 1 && sum <= 5) {
+      alert("it works");
+    } else if (sum >= 6 && sum <= 10) {
+      alert("it don't work");
+    }
   }
 }
